96ac555be1d1b11bcc255d846bb447d0c20fe3f4
Yangfan <syhhsyhk13@gmail.com>
2015-11-28 13:46:53 -0500

    Create kth_smallest.cpp

---
+++ kth_smallest.cpp
@@ -0,0 +1,35 @@
+// k-th order statistic
+// O(nlogn) complexity
+int Solution::kthsmallest(const vector<int> &A, int k) {
+    int len = A.size();
+    vector<int> B = A;
+    sort(B.begin(), B.end());
+    return B[k - 1];
+}
+//
+int Solution::kthSmallest(const vector<int> &A, int k) {
+	vector<int> B = A;
+	int from = 0, to = B.size() - 1;
+	 while (from < to) {
+	  int l = from, r = to;
+	  int mid = B[(l + r) / 2];
+		  while (l < r) {
+			if (B[l] >= mid) { 
+				int tmp = B[r];
+				B[r] = B[l];
+				B[l] = tmp;
+				r--;
+			} else { 
+				l++;
+			}
+		  }
+		if (B[l] > mid)
+			l--;
+		if (k <= l) {
+		to = l;
+		} else {
+		from = l + 1;
+		}
+	}
+	return B[k - 1];
+}
